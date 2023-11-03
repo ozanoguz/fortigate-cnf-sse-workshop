b54aacd710d198c11a97ff914209fc8dd15a5bd6
Ozan Oguz <61783699+ozanoguz@users.noreply.github.com>
2023-11-03 13:58:08 +0300

    Update output.tf

--- terraform-single-vpc/output.tf
+++ terraform-single-vpc/output.tf
@@ -1,40 +1,12 @@
-output "vpc_id" {
-value = "${aws_vpc.main.id}"
-}
-
-output "subnet_id_public-1" {
- value = "${aws_subnet.subnet-public-1.id}"
-}
-
-output "subnet_id_public-2" {
- value = "${aws_subnet.subnet-public-2.id}"
-}
-
-
-output "subnet_id_private-5" {
- value = "${aws_subnet.subnet-private-5.id}"
-}
-
-output "subnet_id_private-6" {
- value = "${aws_subnet.subnet-private-6.id}"
-}
-
-output "subnet_id_private-7" {
- value = "${aws_subnet.subnet-private-7.id}"
-}
-
-output "subnet_id_private-8" {
- value = "${aws_subnet.subnet-private-8.id}"
-}
-
-output "subnet_id_private-9" {
- value = "${aws_subnet.subnet-private-9.id}"
-}
-output "jumbox_public_ip" {
+output "Jumpbox_Public_Ip" {
    value = "${aws_instance.jumpbox.public_ip}"
 }
 
-output "private_key" {
-  value     = tls_private_key.key.private_key_pem
-  sensitive = true
+# Output the public IP address of the FortiAnalyzer instance
+output "FortiAnalyzer_Public_IP" {
+  value = "${aws_instance.fazvm.public_ip}"
+}
+
+output "FortiAnalyzer_Instance_ID" {
+   value = "${aws_instance.fazvm.id}"
 }
