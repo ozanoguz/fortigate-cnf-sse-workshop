f0285a52df23163178f6b9c1226c44f8c75a29cd
Ozan Oguz <61783699+ozanoguz@users.noreply.github.com>
2023-11-03 15:01:19 +0300

    Update output.tf

--- terraform-tgw/output.tf
+++ terraform-tgw/output.tf
@@ -1,4 +1,12 @@
-output "private_key" {
-  value     = tls_private_key.key.private_key_pem
-  sensitive = true
+output "Jumpbox_Public_Ip" {
+   value = "${aws_instance.jumpbox.public_ip}"
+}
+
+# Output the public IP address of the FortiAnalyzer instance
+output "FortiAnalyzer_Public_IP" {
+  value = "${aws_instance.fazvm.public_ip}"
+}
+
+output "FortiAnalyzer_Instance_ID" {
+   value = "${aws_instance.fazvm.id}"
 }
