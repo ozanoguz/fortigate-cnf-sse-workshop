2c597e43c74e64ccf3f263282b146eb8aaf9925b
Ozan Oguz <61783699+ozanoguz@users.noreply.github.com>
2024-06-25 15:50:10 +0300

    Update variables.tf

--- terraform-single-vpc/variables.tf
+++ terraform-single-vpc/variables.tf
@@ -41,19 +41,6 @@ variable "fazami" {
   default = "ami-0e6e65a8b90731e39"
 }
 
-// FortiAnalyzer-VM bootstrap file
-variable "bootstrap_fazvm" {
-  // Change to your own path
-  type    = string
-  default = "fazconfig.conf"
-}
-
-// FortiAnalyzer-VM Flex Token ID
-variable "faz_flextoken" {
-  type = string
-  description = "Enter your FortiAnalyzer Flex token ID"
-}
-
 // FortiAnalyzer-VM hostname
 variable "faz_hostname" {
   type = string
